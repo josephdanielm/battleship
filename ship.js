2828fcb692facc0c1cb0a9363bc92c3da176d4ff
Joseph Mudd <josephmuddjr@gmail.com>
2024-05-01 17:32:59 -0500

    create ship module and initialize constructor

---
+++ ship.js
@@ -0,0 +1,12 @@
+export default class Ship {
+    constructor(length = 1, timesHit = 0, isSunken = false) {
+        this.length = length;
+        this.timesHit = timesHit;
+        this.isSunken = isSunken;
+    }
+
+    
+
+
+}
+
